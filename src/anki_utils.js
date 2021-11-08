6dba8f3da312885f35bc19ebdb9b7a692c765b29
Jonathan Hart <jonathanhart3000@gmail.com>
2021-11-07 20:11:33 -0500

    Added the Anki note method

--- src/anki_utils.js
+++ src/anki_utils.js
@@ -1,10 +1,9 @@
 import fs from 'fs';
 import zip from 'node-zip'
+import os from 'os';
 import sqlite3 from 'sqlite3';
 
-import { open } from 'sqlite';
-
-import { isReadableFile } from './file_utils';
+import * as sqlite from 'sqlite';
 
 /**
  * Unpackages an Anki APKG files and convert it to JSON.
@@ -12,22 +11,21 @@ import { isReadableFile } from './file_utils';
  * This method is adapted from CraigglesO's implementation under the ISC license.
  * See: https://github.com/CraigglesO/anki-to-json.
  * 
- * I have changed it to use the await/async syntax and the sqlite library.
+ * I have changed it to use the await/async syntax and the sqlite library, as well
+ * as to ignore the media files since they aren't super useful here.
+ * 
+ * @async
+ * @param {string} inputFile The input file
+ * @returns {[{ front: string, back: string }]} The Anki notes as JSON
  */
-const convertAnkiToJson = async (inputFile, outputDir) => {
+export const readAnkiNotes = async (inputFile) => {
 
-    // Validate our data
-    if (!inputFile) {
-        throw new Error('An input file must be specified!');
-    }
+    // Create our notes array
+    const notes = [];
 
-    // Get the file name
-    const name = inputFile.split('/').pop().split('.')[0];
-    const dir = (outputDir) ? outputDir : './' + name;
-
-    // Create our output directory if necessary
-    if (!fs.existsSync(dir)) {
-        fs.mkdirSync(dir);
+    // Validate that this actually an APKG file
+    if (!inputFile.endsWith('.apkg')) {
+        throw new Error(`The file ${ankiFilePath} is not a valid Anki APKG file`);
     }
 
     // Read our ZIP file
@@ -42,81 +40,70 @@ const convertAnkiToJson = async (inputFile, outputDir) => {
         throw new Error('Unable to read the Anki database');
     }
 
+    // Get our file name
+    const tmpDir = os.tmpdir();
+    const databaseFile = tmpDir + '/collection.anki2';
+
     // Write this database
-    fs.writeFileSync(dir + '/collection.anki2', ankiCollection._data, {
+    fs.writeFileSync(databaseFile, ankiCollection._data, {
         encoding: 'binary'
     });
 
     // Open this database
-    const ankiDatabase = await open({
-        filename: dir + '/collection.anki2',
+    const ankiDatabase = await sqlite.open({
+        filename: databaseFile,
         driver: sqlite3.Database
     });
     
     // Get our results
     const res = await ankiDatabase.all('SELECT id, flds, sfld FROM notes');
-    const notes = res.map(value => {
-        
-        // Replace some characters
-        const note = {...value};
-        note.front = note.sfld.replace(/\u001f/g, '\n').replace('<div>', '\n').replace('<br>', '\n').replace(/<(?:.|\n)*?>/gm, '');
-        note.back = note.flds.replace(/\u001f/g, '\n').replace('<div>', '\n').replace('<br>', '\n').replace(/<(?:.|\n)*?>/gm, '');
+    if (res) {
+        res.forEach(value => {
+            
+            // Replace some characters
+            const note = {...value};
+            note.front = note.sfld.replace(/\u001f/g, '\n').replace('<div>', '\n').replace('<br>', '\n').replace(/<(?:.|\n)*?>/gm, '');
+            note.back = note.flds.replace(/\u001f/g, '\n').replace('<div>', '\n').replace('<br>', '\n').replace(/<(?:.|\n)*?>/gm, '');
 
-        // Get our open and closed bracket indices for the front
-        const openBracketIndices = [];
-        const closedBrackedIndices = [];
-        for (let i = 0; i < note.front.length; ++i) {
-            if (note.front[i] === '[') openBracketIndices.push(i);
-            if (note.front[i] === ']') closedBrackedIndices.push(i);
-        }
-
-        while (openBracketIndices.length) {
-            const start = openBracketIndices.shift();
-            const end = closedBrackedIndices.shift();
-            const bracketString = note.front.slice(start + 1, end);
-            if (bracketString.includes(':')) {
-                note.front = note.front.slice(0, start) + note.front.slice(end + 1);
+            // Get our open and closed bracket indices for the front
+            const openBracketIndices = [];
+            const closedBrackedIndices = [];
+            for (let i = 0; i < note.front.length; ++i) {
+                if (note.front[i] === '[') openBracketIndices.push(i);
+                if (note.front[i] === ']') closedBrackedIndices.push(i);
             }
-        }
 
-        // Get our open and closed bracket indices for the back
-        for (let i = 0; i < note.back.length; ++i) {
-            if (note.back[i] === '[') openBracketIndices.push(i);
-            if (note.back[i] === ']') closedBrackedIndices.push(i);
-        }
-
-        while (openBracketIndices.length) {
-            const start = openBracketIndices.shift();
-            const end = closedBrackedIndices.shift();
-            const bracketString = note.back.slice(start + 1, end);
-            if (bracketString.includes(':')) {
-                note.back = note.back.slice(0, start) + note.back.slice(end + 1);
+            while (openBracketIndices.length) {
+                const start = openBracketIndices.shift();
+                const end = closedBrackedIndices.shift();
+                const bracketString = note.front.slice(start + 1, end);
+                if (bracketString.includes(':')) {
+                    note.front = note.front.slice(0, start) + note.front.slice(end + 1);
+                }
             }
-        }
 
-        note.front = note.front.trim();
-        note.back = note.back.trim()
-        return note;
-    });
+            // Get our open and closed bracket indices for the back
+            for (let i = 0; i < note.back.length; ++i) {
+                if (note.back[i] === '[') openBracketIndices.push(i);
+                if (note.back[i] === ']') closedBrackedIndices.push(i);
+            }
+
+            while (openBracketIndices.length) {
+                const start = openBracketIndices.shift();
+                const end = closedBrackedIndices.shift();
+                const bracketString = note.back.slice(start + 1, end);
+                if (bracketString.includes(':')) {
+                    note.back = note.back.slice(0, start) + note.back.slice(end + 1);
+                }
+            }
+
+            note.front = note.front.trim();
+            note.back = note.back.trim()
+            notes.push(note);
+        });
+    }
     
     // Remove our dictionary
-    fs.rmSync(dir + '/collection.anki2');
-
-    return '';
-}
-
-export const readAnkiJson = async (ankiFilePath, outputPath) => {
-
-    if (!isReadableFile(ankiFilePath)) {
-        console.error(`Unable to read the Anki file ${ankiFilePath}`);
-        return null;
-    }
-
-    if (!ankiFilePath.endsWith('.apkg')) {
-        console.error(`The file ${ankiFilePath} is not a valid Anki APKG file`);
-        return null;
-    }
-
-    const json = await convertAnkiToJson(ankiFilePath, outputPath);
-    return '{}';
+    fs.rmSync(databaseFile);
+    return notes;
 }
--- src/generate_kanji_list.js
+++ src/generate_kanji_list.js
@@ -1,17 +1,30 @@
-import { readAnkiJson } from "./anki_utils";
+import { readAnkiNotes } from "./anki_utils";
 
-// Generates the Kanji list
+/**
+ * Generates the JLPT kanji list.
+ * 
+ * @param {string} outputDir The output directory
+ * @param {string} pathN5 The N5 Anki path
+ * @param {string} pathN4 The N5 Anki path
+ * @param {string} pathN3 The N3 Anki path
+ * @param {string} pathN2 The N2 Anki path
+ * @param {string} pathN1 The N1 Anki path
+ */
 export const generateKanjiList = async (outputDir, pathN5, pathN4, pathN3, pathN2, pathN1) => {
 
   // Create our Kanji list
   const kanjiList = new Map();
 
   // Read our N5 Kanji
-  const n5Kanji = await readAnkiJson(pathN5, outputDir + '/n5-kanji');
-  if (!n5Kanji) {
-    console.error(`Unable to read the N5 Kanji from ${pathN5}`);
+  try {
+    const notes = await readAnkiNotes(pathN5);
+    notes.forEach(entry => {
+      kanjiList[entry.front] = 5;
+    });
   }
-  else {
-    console.log(n5Kanji);
+  catch {
+    console.error('Unable to read the N5 Kanji');
   }
+
+  console.log(kanjiList);
 }
